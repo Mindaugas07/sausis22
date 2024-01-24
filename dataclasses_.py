df15aed95beb7cf0717a6b73fc472d0e45a17e38
Mindaugas Minelga <minelga.m@gmail.com>
2024-01-24 10:10:09 +0200

    modify dataclasses_.py file

--- dataclasses_.py
+++ dataclasses_.py
@@ -234,71 +234,183 @@ from typing import List
 # print(order_jonas.books)
 
 
-@dataclass
-class Book:
-    title: str
-    author: str
-    publication_year: int
-    isbn: str
+# @dataclass
+# class Book:
+#     title: str
+#     author: str
+#     publication_year: int
+#     isbn: str
+
+
+# @dataclass
+# class Library:
+#     books: List["Book"]
+
+#     def add_book(self, book) -> None:
+#         self.books.append(book)
+
+#     def remove_book(self, book) -> None:
+#         self.books.remove(book)
+
+#     def search_book_by_title(self, name) -> str:
+#         if len(self.books) > 0:
+#             for book in self.books:
+#                 if name in book.title:
+#                     return f"Book '{book.title}' is in the library"
+#                 else:
+#                     return f"Book '{name}' is not present in the library"
+#         else:
+#             return "No books in the library"
+
+#     def search_book_by_author(self, author) -> str:
+#         if len(self.books) > 0:
+#             for book in self.books:
+#                 if author in book.author:
+#                     return f"Book '{book.title}' written by {book.author} is in the library"
+#                 else:
+#                     return f"No book is present in the library written by {author}"
+#         else:
+#             return "No books in the library"
+
+#     def get_books(self) -> List[str]:
+#         return self.books
+
+
+# jungle_book = Book(
+#     title="Jungle Book",
+#     author="Vytautas Jogaila",
+#     publication_year=1945,
+#     isbn="789ty",
+# )
+
+# bible = Book(
+#     title="The Bible",
+#     author="Tom Bairon",
+#     publication_year=12,
+#     isbn="456ret",
+# )
+
+# library_one = Library([])
+# library_one.add_book(bible)
+# print(library_one.get_books())
+# library_one.remove_book(bible)
+# print(library_one.get_books())
+# print(library_one.search_book_by_author("Tom"))
+# print(library_one.search_book_by_author("Tim"))
+# library_one.add_book(bible)
+# library_one.add_book(jungle_book)
+# print(library_one.get_books())
+# print(library_one.search_book_by_author("Tom"))
+# print(library_one.search_book_by_author("Tim"))
 
 
 @dataclass
-class Library:
-    books: List["Book"]
-
-    def add_book(self, book) -> None:
-        self.books.append(book)
-
-    def remove_book(self, book) -> None:
-        self.books.remove(book)
-
-    def search_book_by_title(self, name) -> str:
-        if len(self.books) > 0:
-            for book in self.books:
-                if name in book.title:
-                    return f"Book '{book.title}' is in the library"
-                else:
-                    return f"Book '{name}' is not present in the library"
-        else:
-            return "No books in the library"
-
-    def search_book_by_author(self, author) -> str:
-        if len(self.books) > 0:
-            for book in self.books:
-                if author in book.author:
-                    return f"Book '{book.title}' written by {book.author} is in the library"
-                else:
-                    return f"No book is present in the library written by {author}"
-        else:
-            return "No books in the library"
-
-    def get_books(self) -> List[str]:
-        return self.books
+class Employee:
+    employee_id: int
+    name: str
+    age: str
+    salary: float
+    department: "Department"
 
 
-jungle_book = Book(
-    title="Jungle Book",
-    author="Vytautas Jogaila",
-    publication_year=1945,
-    isbn="789ty",
+@dataclass
+class Department:
+    department_id: int
+    name: str
+    employees: List["Employee"]
+
+    def average_salary(self) -> float:
+        total_dep_salary = 0
+        for index, employee in enumerate(self.employees):
+            total_dep_salary += employee.salary
+        return total_dep_salary / (index + 1)
+
+    def get_name(self) -> str:
+        return self.name
+
+
+@dataclass
+class EmployeeManagement:
+    departments: List["Department"]
+
+    def get_all_employees(self) -> List["Employee"]:
+        all_employees = []
+        for department in self.departments:
+            for employee in department.employees:
+                all_employees.append(employee)
+        return all_employees
+
+    def total_salary(self) -> float:
+        total_comp_salary = 0
+        for department in self.departments:
+            for employee in department.employees:
+                total_comp_salary += employee.salary
+        return total_comp_salary
+
+    def get_employees_in_age_range(
+        self, min_age: int, max_age: int
+    ) -> List["Employee"]:
+        filtered_employees = []
+        all_employees = EmployeeManagement.get_all_employees(self)
+        for employee in all_employees:
+            if min_age <= employee.age <= max_age:
+                filtered_employees.append(employee)
+        return filtered_employees
+
+    def sort_employees_by_salary(self) -> List["Employee"]:
+        sorted_employees_by_salary = []
+        all_employees = EmployeeManagement.get_all_employees(self)
+        for employee in all_employees:
+            sorted_employees_by_salary.append(employee)
+        return sorted(sorted_employees_by_salary, key=lambda d: d.salary, reverse=True)
+
+    def filter_employees_by_department(
+        self, department_name: "Department"
+    ) -> List["Employee"]:
+        filtered_employees_by_department = []
+        all_employees = EmployeeManagement.get_all_employees(self)
+        for employee in all_employees:
+            # print(employee)
+            # print(department_name)
+            if employee.department == department_name:
+                filtered_employees_by_department.append(employee)
+        return filtered_employees_by_department
+
+
+empl_one = Employee(
+    employee_id=123,
+    name="Vardenis Pavardenis",
+    age=45,
+    salary=45000,
+    department="Sales department",
+)
+empl_two = Employee(
+    employee_id=754,
+    name="Vytautas Jogaila",
+    age=19,
+    salary=78000,
+    department="Sales department",
+)
+empl_three = Employee(
+    employee_id=444,
+    name="Algirdas Kestutis",
+    age=60,
+    salary=100000,
+    department="Finances department",
 )
 
-bible = Book(
-    title="The Bible",
-    author="Tom Bairon",
-    publication_year=12,
-    isbn="456ret",
+
+sales_department = Department(
+    department_id=456798, name="Sales department", employees=[empl_one, empl_two]
+)
+financial_department = Department(
+    department_id=789456, name="Finances department", employees=[empl_three]
 )
 
-library_one = Library([])
-library_one.add_book(bible)
-print(library_one.get_books())
-library_one.remove_book(bible)
-print(library_one.get_books())
-print(library_one.search_book_by_author("Tom"))
-print(library_one.search_book_by_author("Tim"))
-library_one.add_book(bible)
-library_one.add_book(jungle_book)
-print(library_one.get_books())
-print(library_one.search_book_by_author("Tom"))
-print(library_one.search_book_by_author("Tim"))
+management = EmployeeManagement(departments=[sales_department, financial_department])
+
+
+# print(management.total_salary())
+# print(sales_department.employees)
+# print(management.sort_employees_by_salary())
+print(management.filter_employees_by_department("Finances department"))
